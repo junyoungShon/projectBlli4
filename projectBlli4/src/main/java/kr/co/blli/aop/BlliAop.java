d5de71e8013b76dad6360b02a9746dbfeed202e7
junyoung <junyoung@39.124.204.130>
2016-02-26 11:37:02 +0900

    d

--- projectBlli4/src/main/java/kr/co/blli/aop/BlliAop.java
+++ projectBlli4/src/main/java/kr/co/blli/aop/BlliAop.java
@@ -24,8 +24,8 @@ public class BlliAop {
 	@Resource
 	private PostingDAO postingDAO;
 	
-	/*@Around("execution(public * kr.co.blli.utility.BlliWordCounter.*(..))")*/
-	@Around("!bean(kr.co.blli.scheduler.*)")
+	@Around("bean(*)")
+	/*@Around("!bean(kr.co.blli.scheduler.*)")*/
 	public Object checkScheduler(ProceedingJoinPoint point){
 		Object retValue= null;
 		Logger logger = Logger.getLogger(getClass());
